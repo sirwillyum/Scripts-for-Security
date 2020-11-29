8532b504721b72a2f8eca6d0847cfe3cab2e29d4
sirwillyum <55957911+sirwillyum@users.noreply.github.com>
2020-11-29 14:29:19 -0500

    Add files via upload

---
+++ logchange.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ls -l /var/log/ | grep Nov | less ##we can see what directories were changed in the month of November as an example for the most recent date. or you can change it to specify the year as well.
